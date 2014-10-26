3a95c9b46591572bdf52e6b1314092ef1dd74469
momoHades <792551849@qq.com>
2014-10-26 14:48:07 +0800

    Merge branch 'deviceManager'

--- src/group503/SystemConcept.java
+++ src/group503/SystemConcept.java
@@ -5,7 +5,7 @@ public class SystemConcept {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		System.out.println("恩坚test method");
+		System.out.println("恩坚test method  #2");
 	}
 
 }
